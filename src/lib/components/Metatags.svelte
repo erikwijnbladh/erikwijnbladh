917602cc4a0c1d58ac2904966300ed7db75a8457
ErikWijnbladh <wijnbladherik@gmail.com>
2024-04-24 18:14:37 +0200

    Update Metatags.svelte

--- src/lib/components/Metatags.svelte
+++ src/lib/components/Metatags.svelte
@@ -10,12 +10,6 @@
   <title>{title}</title>
   <meta name="description" content={description} />
 
-  <meta name="twitter:card" content="summary_large_image" />
-  <meta name="twitter:site" content="www.samcheng.co" />
-  <meta name="twitter:title" content={title} />
-  <meta name="twitter:description" content={description} />
-  <meta name="twitter:image" content={image} />
-
   <meta property="og:title" content={title} />
   <meta property="og:description" content={description} />
   <meta property="og:image" content={image} />
